6296158cb19383f2606c0de94160268aaa347715
Megha619801-creator <mahatomahi2062@gmail.com>
2025-09-10 12:48:28 +0300

    Refactor tours component to use useState for tours array

--- .history/week4-fe-pp/src/components/Tours_20250910103523.jsx
+++ .history/week4-fe-pp/src/components/Tours_20250910103523.jsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
 
 const Tours = () => {
-  // put tours array into state
-  const [toursData, setToursData] = useState(tours);
-
   return (
     <section className="section" id="tours">
-      <Title title="featured" subtitle="tours" />
+      <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
-        {toursData.map((tour) => {
-          return <Tour {...tour} key={tour.id} />;
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />;
         })}
       </div>
     </section>
   );
 };
-
 export default Tours;
