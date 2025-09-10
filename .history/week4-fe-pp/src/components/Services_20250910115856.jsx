6970acb5a3c2744a8f622dbeaa0b2d06643e9443
Megha619801-creator <mahatomahi2062@gmail.com>
2025-09-10 12:34:04 +0300

    Refactor Services component to use useState for services array

--- .history/week4-fe-pp/src/components/Services_20250910115856.jsx
+++ .history/week4-fe-pp/src/components/Services_20250910115856.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
-import { services } from "../data";
-import Title from "./Title";
-import Service from "./Service";
+import { services } from '../data'
+import Title from './Title'
+import Service from './Service'
 
 const Services = () => {
-  // put services array into state
   const [servicesData, setServicesData] = useState(services);
-
   return (
-    <section className="section services" id="services">
-      <Title title="our" subtitle="services" />
+    <section className='section services' id='services'>
+      <Title title='our' subTitle='services' />
 
       <div className="section-center services-center">
         {servicesData.map((service) => {
@@ -17,6 +15,6 @@ const Services = () => {
         })}
       </div>
     </section>
-  );
-};
+  )
+}
 export default Services;
